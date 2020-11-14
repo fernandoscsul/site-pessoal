8677ff1f5559764a38fc44e139fecc7704669159
José Fernando <fernandoscsul@gmail.com>
2020-11-14 00:29:28 -0300

    cores

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 function Home(){
     return (
     <div>
-        <h1>Fernando Gonçales - Dev</h1>
+        <h1 style="color:#006699;">Fernando Gonçales - Dev</h1>
         <Contador/>
     </div>
     )
